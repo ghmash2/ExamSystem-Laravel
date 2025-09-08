ef43ed7eaa3826b3725a668861c28286fc9f6f03
ghmash2 <mahmudpavel74@gmail.com>
2025-09-08 18:02:53 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ExamController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -7,6 +8,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::post('register', AuthController::class);
+// Route::post('login', AuthController::class);
+// Route::get('logout', AuthController::class);
 
 Route::resource('users', UserController::class);
 Route::resource('exams', ExamController::class);
