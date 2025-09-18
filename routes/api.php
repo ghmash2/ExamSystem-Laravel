b9c6842afd05081a890b3425790953a9480bb9d6
ghmash2 <mahmudpavel74@gmail.com>
2025-09-18 18:02:08 +0600

    route update

--- routes/api.php
+++ routes/api.php
@@ -14,26 +14,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/', [ExamController::class, 'index']);
+Route::apiResource('/exams', ExamController::class)->only('index');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/logoutAll', [AuthController::class, 'logoutAll']);
+    Route::get('/exams/{exam}/launch', [LauchExamController::class, 'launch'])->name('exam.luanch');
+    Route::post('/exams/{exam}/submit', [ResultController::class, 'evaluate'])->name('exam.submit');
+    Route::apiResource('/users', UserController::class)->except(['update']);
+    Route::post('/users/{user}', [UserController::class, 'update']);
+    Route::get('/history', [UserController::class, 'history']);
+    Route::get('/single_exam_history/{user_exam}', [UserController::class, 'single_exam_history']);
+    Route::apiResource('/questions', QuestionController::class);
+    Route::apiResource('/options', OptionController::class);
+    Route::apiResource('/departments', DepartmentController::class);
+    Route::apiResource('/students', StudentController::class);
+    Route::apiResource('/exams', ExamController::class)->except('index');
+    Route::post('/exams/add', [CreateExamController::class, 'store']);
 });
 
 
-Route::apiResource('/exams', ExamController::class);
-Route::apiResource('/users', UserController::class)->except(['update']);
-Route::post('/users/{user}', [UserController::class, 'update']);
-Route::get('/history', [UserController::class, 'history']);
-Route::get('/single_exam_history/{user_exam}', [UserController::class, 'single_exam_history']);
-Route::apiResource('/questions', QuestionController::class);
-Route::apiResource('/options', OptionController::class);
-Route::apiResource('/departments', DepartmentController::class);
-Route::apiResource('/students', StudentController::class);
-
-Route::get('/exams/{exam}/launch', [LauchExamController::class, 'launch'])->name('exam.luanch');
-Route::post('/exams/{exam}/submit', [ResultController::class, 'evaluate'])->name('exam.submit');
-Route::post('/exams/add', [CreateExamController::class, 'store']);
 
 // /exam/{exam}/submit  ---> POST evalute return result as response
 // /users/{user}/exams  ----> GET all exams of a user
